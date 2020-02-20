4781c78328c6816a8f823be2549a20007c4abdb7
Nick Thompson <nick.thompson@wwt.com>
2020-02-20 14:29:33 -0600

    updated path

--- bin/mount_minio.sh
+++ bin/mount_minio.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-s3fs sandbox ~/development/devnet-create/snapshots/sandbox -o passwd_file=${HOME}/.passwd-s3fs -o url=http://minio:9000 -o use_path_request_style
+s3fs sandbox ~/development/minio-ansible-pyats-sandbox/snapshots/sandbox -o passwd_file=${HOME}/.passwd-s3fs -o url=http://minio:9000 -o use_path_request_style
